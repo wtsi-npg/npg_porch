786118fef650657955e67e14a58a1ee7402ab1d2
Kieron Taylor <kt19@sanger.ac.uk>
2022-01-14 14:46:07 +0000

    Extract schema settings for ORM, so unit testing can leave it unset

--- server/deploy_schema.py
+++ server/deploy_schema.py
@@ -6,7 +6,11 @@ import sqlalchemy
 import npg.porchdb.models
 
 db_url = os.environ.get('DB_URL')
+schema_name = os.environ.get('DB_SCHEMA')
+if schema_name is None:
+    schema_name = 'npg_porch'
 
 engine = sqlalchemy.create_engine(db_url)
 
+npg.porchdb.models.Base.metadata.schema = schema_name
 npg.porchdb.models.Base.metadata.create_all(engine)
