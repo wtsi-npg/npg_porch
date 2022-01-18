dc0db4c90b97aa732db5aa59f4bf3abba6323f06
Kieron Taylor <kt19@sanger.ac.uk>
2022-01-18 16:20:55 +0000

    Update test conf

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,2 +1,2 @@
 from .fixtures.orm_session import sync_session, async_session
-from .fixtures.deploy_db import sync_minimum, async_minimum, minimum_data
+from .fixtures.deploy_db import sync_minimum, async_minimum, minimum_data, fastapi_testclient
