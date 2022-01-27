2e667b350808c1afc4de162c27d2c6deed22597d
mgcam <mg8@sanger.ac.uk>
2022-01-27 16:11:11 +0000

    Added a test for the Task ORM model

---
+++ server/tests/db_task_test.py
@@ -0,0 +1,26 @@
+import pytest
+from sqlalchemy import select
+
+from .fixtures.deploy_db import async_minimum
+from npg.porchdb.models import Task
+
+@pytest.mark.asyncio
+async def test_task_creation(async_minimum):
+    ''''
+    Test task creation from a fixture.
+    Relationships test.
+    '''
+
+    result = await async_minimum.execute(
+        select(Task)
+    )
+    tasks = result.scalars().all()
+    assert len(tasks) == 2
+    task_a = tasks[0]
+    assert task_a.job_descriptor == 'aaaaaaaaa'
+    assert task_a.pipeline.repository_uri == 'pipeline-test.com'
+    events = task_a.events
+    assert len(events) == 1
+    assert events[0].change == 'Created'
+    assert events[0].token is not None
+
