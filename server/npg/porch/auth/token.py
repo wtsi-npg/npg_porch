2b21539abd47c2ca218b26b5d117d9a60ed80764
mgcam <mg8@sanger.ac.uk>
2022-03-09 19:09:12 +0000

    Copyright notices added/fixed

--- server/npg/porch/auth/token.py
+++ server/npg/porch/auth/token.py
@@ -1,3 +1,23 @@
+# Copyright (C) 2022 Genome Research Ltd.
+#
+# Author: Kieron Taylor kt19@sanger.ac.uk
+# Author: Marina Gourtovaia mg8@sanger.ac.uk
+#
+# This file is part of npg_porch
+#
+# npg_porch is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
 import logging
 from fastapi import Depends
 from fastapi.security import HTTPBearer
@@ -19,7 +39,7 @@ async def validate(
         p = await validator.token2permission(token)
     except CredentialsValidationException as e:
         logger = logging.getLogger(__name__)
-        logger.warning(e)
+        logger.warning(str(e))
         raise HTTPException(status_code=403, detail="Invalid token")
 
     return p
